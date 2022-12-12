890db679c97b13487b9e4535d36cc0eac3fd9fcd
brian <kipkoechbrian0@gmail.com>
2022-12-12 15:45:16 +0300

    implement action reducer and dispatch

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,28 @@
 // add code snippets from README
+let state;
+const button = document.querySelector("#button");
+
+function changeState(action, state = { count: 0 }) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    case "counter/decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+function render(params) {
+  document.querySelector("#container").textContent = state.count;
+}
+function dispatch(action) {
+  state = changeState(action, state);
+  render();
+}
+
+dispatch({ type: "@@INIT" });
+
+button.addEventListener("click", () => {
+  dispatch({ type: "counter/increment" });
+});
